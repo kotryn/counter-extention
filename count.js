f663adb924909ca9ac54daa10c312c7b43afa0a3
katarzyna <k.romaszewska@openmobi.pl>
2019-04-18 11:29:02 +0200

    ver 2 - count hours min seconds

--- count.js
+++ count.js
@@ -6,16 +6,41 @@ function getRandomColor() {
   }
   return color;
 }
-  
-var oneDay = 24 * 60 * 60 * 1000;
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-var firstDate = new Date(yyyy, mm, dd);
-var secondDate = new Date(2019, 04, 18);
-var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+
+function countDate() {
+  var today = new Date();
+  var yyyy = today.getFullYear();
+  var dd = today.getDate()
+  var mm = today.getMonth() + 1
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+
+  var nowDate = new Date(yyyy, mm, dd, h, m, s).getTime();
+  var endDate = new Date(2019, 4, 18, 23, 45, 0).getTime();
+  var timeRemaining = parseInt((endDate - nowDate) / 1000);
+
+  if (timeRemaining >= 0) {
+    days = parseInt(timeRemaining / 86400);
+    timeRemaining = (timeRemaining % 86400);
+
+    hours = parseInt(timeRemaining / 3600);
+    timeRemaining = (timeRemaining % 3600);
+
+    minutes = parseInt(timeRemaining / 60);
+    timeRemaining = (timeRemaining % 60);
+
+    seconds = parseInt(timeRemaining);
+  }
+
+  return hours + "h " + minutes + "min " + seconds + "s";
+}
+
 var res = document.querySelector('#res');
 res.style.color = getRandomColor();
 document.body.style.backgroundColor = getRandomColor();
-res.innerHTML = diffDays;
+res.innerHTML = countDate();
+
+setInterval(function () {
+  res.innerHTML = countDate();
+}, 1000)
